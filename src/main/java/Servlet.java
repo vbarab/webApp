62aa7e0589cb28f352c498421c12455b905815e8
Vitaly <vbarabancev@gmail.com>
2015-02-26 22:14:20 +0600

    added sm files

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -4,10 +4,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 @WebServlet(name = "Servlet")
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        {
+            final Writer writer = new Writer() {
+                @Override
+                public void write(char[] cbuf, int off, int len) throws IOException {
+
+                }
+
+                @Override
+                public void flush() throws IOException {
+
+                }
+
+                @Override
+                public void close() throws IOException {
+
+                }
+            };
+            writer.write("asdasd");
+        }
 
     }
 
