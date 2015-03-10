8bf4ae627acfec9de186b30f93619aff8c095e37
Vitaly <vbarabancev@gmail.com>
2015-03-11 04:56:20 +0600

    PostFactory

--- src/main/java/com/epam/vb/entity/Blog/factory/PostFactory.java
+++ src/main/java/com/epam/vb/entity/Blog/factory/PostFactory.java
@@ -1,13 +1,41 @@
 package com.epam.vb.entity.Blog.factory;
 
+import com.epam.vb.entity.Blog.model.Authorized;
+import com.epam.vb.entity.Blog.model.Blog;
 import com.epam.vb.entity.Blog.model.Post;
+import com.epam.vb.entity.Blog.model.Tag;
+import com.thedeanda.lorem.Lorem;
+import javafx.geometry.Pos;
 
-import java.util.Date;
-import java.util.Random;
-import java.util.UUID;
+import javax.swing.text.html.HTML;
+import java.util.*;
 
 public class PostFactory {
     private static final Random RANDOM = new Random();
+
+    public static List<Post> createList(int amount, Blog blog, Authorized authorized, List<Tag> tags) {
+        List<Post> posts = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            posts.add(create(blog, authorized, tags));
+        }
+        return posts;
+    }
+
+    public static Post create(Blog blog, Authorized authorized, List<Tag> tags) {
+        Post post = new Post();
+        post.setAuthorName(Lorem.getName());
+        post.setTitle(Lorem.getTitle(7));
+        post.setBodyText(Lorem.getWords(90));
+        List<Tag> postTags = new ArrayList<>();
+        for (Tag tag : tags) {
+            if (Math.random() < 0.5) postTags.add(tag);
+        }
+        post.setTagList(postTags);
+        post.setViews((int) Math.random() * 1000);
+        post.setLike((int) Math.random() * 70);
+        post.setDeleted(Math.random() > 5);
+        return post;
+    }
 
     public static String stringGenerator(String characters, int length) {
 
@@ -34,13 +62,13 @@ public class PostFactory {
         return new UUID(RANDOM.nextInt(), RANDOM.nextInt());
     }
 
-    public static Post createRandomPost() {
+/*    public static Post createRandomPost() {
          String name = nameCreator();
          String author = authorCreator();
          Date date = dateCreator();
          UUID uuid = uuidCreator();
         return new Post(name, author, date, uuid);
 
-    }
+    }*/
 
 }
